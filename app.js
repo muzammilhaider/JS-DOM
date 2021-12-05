7ef78364f320c2069939330f5eab9a6346f89045
Muzammil Haider <muzammilhaider110@gmail.com>
2021-12-05 16:38:49 +0500

    DOM Objects

---
+++ app.js
@@ -0,0 +1,53 @@
+// ques 1
+// ans i
+// var mainContent = document.getElementById('main-content');
+
+// ans ii
+// var mainContent = document.getElementById('main-content');
+// console.log(mainContent.children);
+
+// ans iii
+// var renderClass = document.getElementsByClassName('render');
+// for(var i = 0; i < renderClass.length; i++){
+//     document.write(renderClass[i].innerHTML);
+// }
+
+// ans iv
+// var firstName = document.getElementById('first-name');
+// firstName.value = "Muzammil";
+
+// ans v
+// var lastName = document.getElementById('last-name');
+// lastName.value = "Haider";
+// var email = document.getElementById('email');
+// email.value = "muzammil@gmail.com";
+
+// ques 2
+// ans i
+// var formContent = document.getElementById('form-content');
+// console.log(formContent.nodeType);
+
+// ans ii
+// var lastName = document.getElementById('lastName');
+// console.log(lastName.nodeType);
+
+// ans iii
+// var lastName = document.getElementById('lastName');
+// lastName.childNodes[0] = "last name";
+// lastName.childNodes[0].replaceChild("last names",lastName.childNodes[0]);
+// console.log(lastName.childNodes[0]);
+
+// ans iv
+// var mainContent = document.getElementById('main-content');
+// console.log(mainContent.firstElementChild);
+// console.log(mainContent.lastElementChild);
+
+// ans v
+// var lastName = document.getElementById('lastName');
+// console.log(lastName.nextElementSibling);
+// console.log(lastName.previousElementSibling);
+
+// ans vi
+// var email = document.getElementById('email');
+// console.log(email.parentNode);
+// console.log(email.nodeType);
